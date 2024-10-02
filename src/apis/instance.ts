b7205b09e9a49c6351645ac467100040f0a5e339
shimseohyun <shim020924@gmail.com>
2024-10-02 16:53:10 +0900

    [Featuer] #35 - Setting Base URL env

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,17 +1,25 @@
 import axios, { AxiosError } from "axios";
 
 const instance = axios.create({
-  baseURL: "",
+  baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: false, //크로스 도메인 요청 시 쿠키, HTTP 인증 및 클라이언트 SSL 인증서를 사용하도록 허용한다.
 });
 
 export const getResponse = async <T>(url: string): Promise<T | null> => {
   try {
-    const response = await instance.get<T>(url);
+    console.log(`[GET] ${url}\n`);
+    const response = await instance.get<T>(url, {
+      headers: {
+        Authorization: `Bearer `,
+      },
+    });
+
+    console.log(response);
+
     return response.data;
   } catch (error) {
     const axiosError = error as AxiosError;
-    console.log(`[GET] ${url}`);
+
     console.error("Response error:", axiosError);
     return null;
   }
