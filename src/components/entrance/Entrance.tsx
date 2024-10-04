9662ce50138df9cdf7091c92809a2e8f7ad8225f
sayyyho <323psh@naver.com>
2024-10-04 21:22:35 +0900

    [Refactor] #47 - Entry logic refactoring

--- src/components/entrance/Entrance.tsx
+++ src/components/entrance/Entrance.tsx
@@ -1,26 +1,33 @@
 import { useNavigate } from "react-router-dom";
-import useModal from "@hooks/useModal";
 import Button from "@components/button/Button";
 import ButtonLayout from "@components/button/ButtonLayout";
 import BoothCardDetail from "@components/boothCard/boothCardDetail";
 import * as S from "./Entrance.styled";
+import {
+  handleConfirmEntry,
+  handleCancelEntry,
+  getThreeMinutesLater,
+} from "./entranceUtils";
 import useEntranceBottomsheet from "@hooks/useEntrance";
-import useTimer from "@hooks/useTimer";
-// import { useAtom } from "jotai";
-// import { entranceBottomsheetAtom } from "@atoms/entrance";
+import useModal from "@hooks/useModal";
+import useCountdown from "@hooks/useCountdown";
+
 export interface EntranceProps {
   boothID?: number;
   isOpen?: boolean;
   nextPath: string;
 }
-// const navigate = useNavigate();
 
 export const Entrance = ({ boothID, nextPath }: EntranceProps) => {
   const navigate = useNavigate();
   const { closeEntrace } = useEntranceBottomsheet();
   const { openModal, closeModal } = useModal();
-  const { minutes, seconds } = useTimer(3, 0);
-  // const [entranceState] = useAtom(entranceBottomsheetAtom);
+  // const targetDate = ;
+
+  // targetDate를 useCountdown 훅에 전달
+  const { getTime, isCountdownOver } = useCountdown({
+    targetDate: getThreeMinutesLater(),
+  });
 
   return (
     <S.EntranceWrapper>
@@ -40,55 +47,25 @@ export const Entrance = ({ boothID, nextPath }: EntranceProps) => {
       <ButtonLayout $col={1}>
         <Button
           scheme="lime"
-          disabled={false}
+          disabled={isCountdownOver} // 타이머 종료 시 버튼 비활성화
           onClick={() =>
-            openModal({
-              title: "다른 대기가 취소돼요",
-              sub: "입장을 확정하면 다른 대기는 취소돼요.\n입장을 확정하시겠어요?",
-              secondButton: {
-                children: "이전으로",
-                onClick: () => closeModal(),
-              },
-              primaryButton: {
-                children: "입장 확정하기",
-                scheme: "lime",
-                onClick: () => {
-                  closeModal();
-                  navigate(
-                    `${nextPath.startsWith("/") ? nextPath : `/${nextPath}`}`
-                  );
-                  // jotai 체크 확인
-                },
-              },
-            })
+            handleConfirmEntry(
+              openModal,
+              closeModal,
+              closeEntrace,
+              navigate,
+              nextPath
+            )
           }
         >
           <span>입장할게요!</span>
-          <span>
-            {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-          </span>
+          <span>{getTime("MMSS")}</span>
         </Button>
         <Button
           scheme="grayLight"
           disabled={false}
           shape="outline"
-          onClick={() =>
-            openModal({
-              title: "정말 입장을 취소하시겠어요?",
-              sub: "입장을 취소하면 현재 줄 서기가 사라져요.\n그래도 취소하실건가요?",
-              secondButton: {
-                children: "이전으로",
-                onClick: () => closeModal(),
-              },
-              primaryButton: {
-                children: "입장 취소하기",
-                onClick: () => {
-                  closeModal();
-                  closeEntrace();
-                },
-              },
-            })
-          }
+          onClick={() => handleCancelEntry(openModal, closeModal, closeEntrace)}
         >
           <span>입장 취소하기</span>
         </Button>
